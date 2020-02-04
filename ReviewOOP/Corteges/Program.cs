d24b7357d5111d62a97c86aa748baeae23166a49
user <user@DESKTOP-SFHS1DK>
2020-02-04 13:30:54 +0400

    Corteges

---
+++ ReviewOOP/Corteges/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Corteges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Example1();
+
+            //Example2();
+        }
+
+        private static void Example2()
+        {
+            var (name, age) = ("Jerry", 7);
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+        }
+
+        private static void Example1()
+        {
+            var cort = ("Vahe", 'A');
+            cort.Item1 = "Tom";
+            cort.Item2 = '?';
+
+            Console.WriteLine($"We view new values. Value1;{cort.Item1}   Value2:{cort.Item2}");
+            Console.ReadLine();
+        }
+    }
+}
