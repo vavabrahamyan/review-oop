32e353555e79b285d4f5a978e15ee148cbb4ebde
user <user@DESKTOP-SFHS1DK>
2020-02-04 13:51:02 +0400

    Ex

---
+++ ReviewOOP/Exceptions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try...catch...finally
+
+            int[] nums = new int[4];
+            nums[4] = 10;//Error 
+
+            try
+            {
+                nums[4] = 10;
+                Console.WriteLine("End of try block");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error 404: {ex.Message}");
+                Console.WriteLine($"temporary problems...");
+            }
+            finally
+            {
+                Console.WriteLine($"Block finally");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
