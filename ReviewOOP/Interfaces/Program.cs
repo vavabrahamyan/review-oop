886d18aa83836c8a3379af3012b0ef7551db9de9
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-02-13 18:57:52 +0400

    2

---
+++ ReviewOOP/Interfaces/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //about IEnumerable, ICollection and IList
+            //IList is inherited of ICollection and IEnumerable
+            //ICollection is inherited of IEnumerable 
+                //Methodes and fields in IList
+                     //Indexator, IsFixedSize, IsReadonly,
+                     //Add(), Clear(), Contains(), IndexOf(),
+                     //Insert(), Remove(), RemoveAt,
+                //Methodes and fields in ICollection
+                    //Count, IsSynchronized, SyncRoot
+                    //CopyTo()
+                //Methodes and fields in IEnumerable
+                    //GetEnumerator(),
+        }
+    }
+}
