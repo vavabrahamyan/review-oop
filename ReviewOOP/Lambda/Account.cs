10dab5a96a0a28c3eed1c34aa6817936e3bed902
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-03-08 18:12:45 +0400

    802_c

---
+++ ReviewOOP/Lambda/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    delegate void AccountStateHandler(object sender, AccountEventArgs e);
+    class Account
+    {
+        int _sum;
+
+        public Account(int sum)
+        {
+            _sum = sum;
+        }
+
+        public event AccountStateHandler Added;
+        public event AccountStateHandler Withdrawn;
+
+        public void Put(int sum)
+        {
+            _sum += sum;
+            if (Added != null)
+                Added(this, new AccountEventArgs($"In balance added {sum}", sum));
+        }
+        public void Withdraw(int sum)
+        {
+            if(_sum >= sum)
+            {
+                _sum -= sum;
+                if (Withdrawn != null)
+                    Withdrawn(this, new AccountEventArgs($"Withdraw of balance{sum}", sum));
+
+            }
+        }
+    }
+    class AccountEventArgs
+    {
+        public string Message { get; }
+        public int Sum { get; }
+        public AccountEventArgs(string message, int sum)
+        {
+            Message = message;
+            Sum = sum;
+        }
+    }
+}
---
+++ ReviewOOP/Lambda/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        delegate int Operation(int x, int y);
+        public delegate void Deleg();
+        public delegate void Display(string str);
+        static void Main(string[] args)
+        {
+            Account account = new Account(100);
+            account.Added += (sender, e) =>
+            {
+                Console.WriteLine(e.Sum);
+                Console.WriteLine(e.Message);
+            };
+
+            account.Put(100);
+            account.Put(203);
+
+
+            //Display display = (p) => Console.WriteLine(p.ToString());
+            //display("Hello World");
+
+            //Deleg d = () => Console.WriteLine("Hello axper jan");
+            //d.Invoke();
+            //Operation op1 = (x, y) => x + y;
+            //int z = op1(11, 111);
+        }
+    }
+}
