7f11864023cfb76ed3ca5486bb173ffe702e7c9c
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-03-08 21:06:29 +0400

    805_c

--- ReviewOOP/Structs/Program.cs
+++ ReviewOOP/Structs/Program.cs
@@ -6,8 +6,20 @@ namespace Structs
     {
         static void Main(string[] args)
         {
-            A a = new A();
-            Console.WriteLine(a.Num);
+            UserInfo ui_1 = new UserInfo("Vahe", 25);
+            UserInfo ui_2 = new UserInfo("Mariam", 20);
+
+            ui_1.Display();
+            ui_2.Display();
+
+            Console.WriteLine("After change");
+            ui_2 = ui_1;
+
+            ui_1.Name = "Mariamcho";
+            ui_1.Age = 19;
+
+            ui_1.Display();
+            ui_2.Display();
         }
     }
 }
---
+++ ReviewOOP/Structs/UserInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    struct UserInfo
+    {
+        public string Name;
+        public byte Age;
+
+        public UserInfo(string name, byte age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"{Name} is {Age} years old");
+        }
+    }
+}
