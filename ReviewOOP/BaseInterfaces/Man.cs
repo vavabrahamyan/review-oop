eea5a47a68e0814aa6871ba22bca45404003a86c
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-09-08 21:05:01 +0400

    ~4

---
+++ ReviewOOP/BaseInterfaces/Man.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseInterfaces
+{
+    class Man:IComparable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Person p = obj as Person;
+            if(p != null)
+            {
+                return this.Name.CompareTo(p.Name);
+            }
+            else
+            {
+                Console.WriteLine("Error 444");
+                return -1;
+            }
+        }
+    }
+}
--- ReviewOOP/BaseInterfaces/Program.cs
+++ ReviewOOP/BaseInterfaces/Program.cs
@@ -6,14 +6,26 @@ namespace BaseInterfaces
     {
         static void Main(string[] args)
         {
-            Person p1 = new Person { Name = "Tom", Age = 27, Work = new Company { Name = "Microsoft" } };
-            Person p2 = (Person)p1.Clone();
-            //Person p3 = p1;
-            p2.Name = "Alice";
-            p2.Work = new Company { Name = "Google" };
-            //p3.Name = "George";
-            Console.WriteLine(p1.Name);
-            Console.WriteLine(p1.Work.Name);
+            //Person p1 = new Person { Name = "Tom", Age = 27, Work = new Company { Name = "Microsoft" } };
+            //Person p2 = (Person)p1.Clone();
+            ////Person p3 = p1;
+            //p2.Name = "Alice";
+            //p2.Work = new Company { Name = "Google" };
+            ////p3.Name = "George";
+            //Console.WriteLine(p1.Name);
+            //Console.WriteLine(p1.Work.Name);
+
+            Man m1 = new Man { Name = "Bill", Age = 44 };
+            Man m2 = new Man { Name = "Stive", Age = 34 };
+            Man m3 = new Man { Name = "Alice", Age = 17 };
+
+            Man[] men = new Man[] { m1, m2, m3 };
+            Array.Sort(men);
+            foreach (var m in men)
+            {
+                Console.WriteLine($"{m.Name} __ {m.Age}");
+            }
+            Console.Read();
         }
     }
 }
