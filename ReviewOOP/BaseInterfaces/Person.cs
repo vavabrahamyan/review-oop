b13a8abb0dcfcc6ddcd7b78582e0fcb9b0b5a74e
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-09-08 20:47:46 +0400

    ~3

---
+++ ReviewOOP/BaseInterfaces/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseInterfaces
+{
+    class Person : ICloneable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Company Work { get; set; }
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+    class Company
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ ReviewOOP/BaseInterfaces/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BaseInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person { Name = "Tom", Age = 27, Work = new Company { Name = "Microsoft" } };
+            Person p2 = (Person)p1.Clone();
+            //Person p3 = p1;
+            p2.Name = "Alice";
+            p2.Work = new Company { Name = "Google" };
+            //p3.Name = "George";
+            Console.WriteLine(p1.Name);
+            Console.WriteLine(p1.Work.Name);
+        }
+    }
+}
